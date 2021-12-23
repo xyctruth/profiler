8d73303ed6780d759d783c8299e5848c652068d7
toozhuang <wangz.today@gmail.com>
2021-12-23 14:34:41 +0800

    feat: UI 简单适配 Mobile 视图

--- ui/src/view/index/index.vue
+++ ui/src/view/index/index.vue
@@ -1,40 +1,54 @@
 <template>
   <div class='index container'>
-    <el-row :gutter="20">
-      <el-col :span="20">
+    <el-row :gutter="22">
+      <el-col>
         <el-card style="margin-bottom: 20px;">
-          <div flex="cross:center main:justify">
-            <div>
-              <selectTypes style="margin-right: 20px;" v-model:selectTypes="types"></selectTypes>
-              <selectProject style="margin-right: 20px;" v-model:selectProjects="projects"></selectProject>
-            </div>
-            <div>
-              <selectTimeRange v-model:timeRange="timeRange"></selectTimeRange>
-            </div>
-          </div>
+          <!--          <div flex="cross:center main:justify">-->
+
+          <el-row :gutter="10">
+            <!--              这里按照视图分层-->
+
+            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+              <selectTypes style="margin-right: 20px; width: 90%" v-model:selectTypes="types"></selectTypes>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+              <selectProject style="margin-right: 20px;width: 90%" v-model:selectProjects="projects"></selectProject>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+              <selectTimeRange class="time-range-container" v-model:timeRange="timeRange"
+                               style="width: 90%"></selectTimeRange>
+            </el-col>
+          </el-row>
+          <!--          </div>-->
+
         </el-card>
         <div>
           <el-card
-            class="char-card"
-            v-for="(item,index) in types"
-            :key="index"
-            style="margin-bottom: 30px;">
+              class="char-card"
+              v-for="(item,index) in types"
+              :key="index"
+              style="margin-bottom: 30px;overflow: auto;overflow: scroll  ">
             <div :id="item"></div>
             <chart
-              :index="index"
-              :type="item"
-              :projects="projects"
-              :timeRange="timeRange"></chart>
+                style="min-width: 1200px;"
+                :index="index"
+                :type="item"
+                :projects="projects"
+                :timeRange="timeRange"></chart>
           </el-card>
         </div>
       </el-col>
-      <el-col :span="4">
+    </el-row>
+    <el-row>
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
         <el-affix :offset="20" class="guid">
           <el-card>
             <el-button
-              @click="jump(item)"
-              v-for="(item,index) in types" :key="index" type="text">
-              {{item}}
+                @click="jump(item)"
+                v-for="(item,index) in types" :key="index" type="text"
+                style="display: inline-block;margin-right: 20px;"
+            >
+              {{ item }}
             </el-button>
           </el-card>
         </el-affix>
@@ -45,32 +59,45 @@
 </template>
 
 <script setup>
-  import {ref} from 'vue'
-  import selectTypes from './components/selectTypes.vue'
-  import selectProject from './components/selectProject.vue'
-  import selectTimeRange from './components/selectTimeRange.vue'
-  import chart from './components/chart.vue'
+import {ref} from 'vue'
+import selectTypes from './components/selectTypes.vue'
+import selectProject from './components/selectProject.vue'
+import selectTimeRange from './components/selectTimeRange.vue'
+import chart from './components/chart.vue'
 
-  const types = ref([])
-  const projects = ref([])
-  const timeRange = ref([])
-  const jump = (item) => {
-    document.getElementById(item).scrollIntoView()
-  }
+console.log('local')
+const types = ref([])
+const projects = ref([])
+const timeRange = ref([])
+const jump = (item) => {
+  document.getElementById(item).scrollIntoView()
+}
 </script>
 
 <style lang="scss" scoped>
-  .guid {
-    .el-button {
-      margin-left: 0;
-      display: block;
-    }
+.guid {
+  .el-button {
+    margin-left: 0;
+    display: block;
   }
+}
 </style>
 <style lang="scss">
-  .char-card {
-    .el-card__body {
-      padding: 20px 0;
-    }
+.char-card {
+  .el-card__body {
+    padding: 20px 0;
   }
+}
+
+.el-col {
+  margin-bottom: 10px;
+}
+
+.time-range-container {
+  .el-input {
+    width: 100%;
+  }
+}
+
+
 </style>
