bedf5dca9da64f9f63c78e9f3f3af9be00bf3b81
toozhuang <wangz.today@gmail.com>
2021-12-23 17:19:25 +0800

    feat: 更改 PC 端导航栏位置到右边

--- ui/src/view/index/index.vue
+++ ui/src/view/index/index.vue
@@ -1,59 +1,67 @@
 <template>
   <div class='index container'>
     <el-row :gutter="22">
-      <el-col>
-        <el-card style="margin-bottom: 20px;">
-          <!--          <div flex="cross:center main:justify">-->
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="20" >
+        <el-row :gutter="22" >
+          <el-col >
+            <el-card  style="margin-bottom: 20px;margin-top:10px">
+              <!--          <div flex="cross:center main:justify">-->
 
-          <el-row :gutter="10">
-            <!--              这里按照视图分层-->
+              <el-row :gutter="10">
+                <!--              这里按照视图分层-->
 
-            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
-              <selectTypes style="margin-right: 20px; width: 90%" v-model:selectTypes="types"></selectTypes>
-            </el-col>
-            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
-              <selectProject style="margin-right: 20px;width: 90%" v-model:selectProjects="projects"></selectProject>
-            </el-col>
-            <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
-              <selectTimeRange class="time-range-container" v-model:timeRange="timeRange"
-                               style="width: 90%"></selectTimeRange>
-            </el-col>
-          </el-row>
-          <!--          </div>-->
+                <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+                  <selectTypes style="margin-right: 20px; width: 90%" v-model:selectTypes="types"></selectTypes>
+                </el-col>
+                <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+                  <selectProject style="margin-right: 20px;width: 90%" v-model:selectProjects="projects"></selectProject>
+                </el-col>
+                <el-col :xs="24" :sm="24" :md="8" :lg="9" :xl="8">
+                  <selectTimeRange class="time-range-container" v-model:timeRange="timeRange"
+                                   style="width: 90%"></selectTimeRange>
+                </el-col>
+              </el-row>
+              <!--          </div>-->
 
-        </el-card>
-        <div>
-          <el-card
-              class="char-card"
-              v-for="(item,index) in types"
-              :key="index"
-              style="margin-bottom: 30px;overflow: auto;overflow: scroll  ">
-            <div :id="item"></div>
-            <chart
-                style="min-width: 1200px;"
-                :index="index"
-                :type="item"
-                :projects="projects"
-                :timeRange="timeRange"></chart>
-          </el-card>
-        </div>
+            </el-card>
+            <div>
+              <el-card
+                  class="char-card"
+                  v-for="(item,index) in types"
+                  :key="index"
+                  style="margin-bottom: 30px;overflow: auto;overflow: scroll  ">
+                <div :id="item"></div>
+                <chart
+                    style="min-width: 1200px;"
+                    :index="index"
+                    :type="item"
+                    :projects="projects"
+                    :timeRange="timeRange"></chart>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="4">
+        <el-row>
+          <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+            <el-affix :offset="10" class="guid">
+              <el-card>
+                <el-button
+                    @click="jump(item)"
+                    v-for="(item,index) in types" :key="index" type="text"
+                    style="display: inline-block;padding-right: 20px;"
+                >
+                  {{ item }}
+                </el-button>
+              </el-card>
+            </el-affix>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-        <el-affix :offset="20" class="guid">
-          <el-card>
-            <el-button
-                @click="jump(item)"
-                v-for="(item,index) in types" :key="index" type="text"
-                style="display: inline-block;margin-right: 20px;"
-            >
-              {{ item }}
-            </el-button>
-          </el-card>
-        </el-affix>
-      </el-col>
-    </el-row>
+
 
   </div>
 </template>
